262372ae55ac1be1e4fcd49b73ac8fc686d0c0e8
Severussss <farhanraldifan10@gmail.com>
2026-02-12 22:27:39 +0700

    stream commit

--- stream-sentiment.py
+++ stream-sentiment.py
@@ -5,12 +5,5 @@ nb_model = pickle.load(open('NB_model.sav', 'rb'))
 
 st.title("Sentiment Analysis with Naive Bayes")
 
-st.image("sentiment_polarity.jpg", caption="Sentiment Polarity", use_column_width=True)
-
-user_input = st.text_area("Enter text for sentiment analysis:")
-if st.button("Analyze Sentiment"):
-    prediction = nb_model.predict([user_input])
-    st.write(f"Predicted Sentiment: {prediction[0]}")
-else:
-    st.write("Please enter some text to analyze.")  
+st.image("sentiment_polarity.jpg", caption="Sentiment Polarity"))
     
