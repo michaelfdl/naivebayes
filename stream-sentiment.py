6dea8c335d227b4c821a2d1c95a69281cb986248
Severussss <farhanraldifan10@gmail.com>
2026-02-12 22:28:26 +0700

    stream commit second

--- stream-sentiment.py
+++ stream-sentiment.py
@@ -5,5 +5,5 @@ nb_model = pickle.load(open('NB_model.sav', 'rb'))
 
 st.title("Sentiment Analysis with Naive Bayes")
 
-st.image("sentiment_polarity.jpg", caption="Sentiment Polarity"))
+st.image("sentiment_polarity.jpg", caption="Sentiment Polarity")
     
